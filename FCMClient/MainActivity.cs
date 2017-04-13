2824703ea6193b7ef06804c79e3b07abcfbd1c2a
LeanManager <zwabyapp@gmail.com>
2017-04-13 09:54:20 -0400

    Initial check-in of module FCMClient

---
+++ FCMClient/MainActivity.cs
@@ -0,0 +1,93 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Common;
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
+
+namespace FCMClient
+{
+	[Activity(Label = "FCMClient", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+        TextView msgText;
+        const string TAG = "MainActivity";
+
+       /* IsPlayServicesAvailable is called at the end of OnCreate so that the Google Play Services 
+          check runs each time the app starts. If your app has an OnResume method, 
+          it should call IsPlayServicesAvailable from OnResume as well. */
+		
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+	        SetContentView(Resource.Layout.Main);
+
+            msgText = FindViewById<TextView>(Resource.Id.msgText);
+
+            /* When the user taps a notification issued from FCMClient, any data accompanying that 
+			   notification message is made available in Intent extras. */
+			
+			if (Intent.Extras != null)
+            {
+                foreach (var key in Intent.Extras.KeySet())
+                {
+                    var value = Intent.Extras.GetString(key);
+                    Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
+                }
+            }
+
+	        IsPlayServicesAvailable();
+
+            // This code logs the current token to the output window when the Log Token button is tapped.
+            var logTokenButton = FindViewById<Button>(Resource.Id.logTokenButton);
+
+            logTokenButton.Click += delegate 
+			{ 
+				Log.Debug(TAG, "InstanceID token: " + FirebaseInstanceId.Instance.Token); 
+			};
+
+            /* This code locates the Subscribe to Notification button in the layout and assigns its click 
+			   handler to code that calls FirebaseMessaging.Instance.SubscribeToTopic, passing in the subscribed 
+			   topic, news. When the user taps the Subscribe button, the app subscribes to the news topic. */
+            var subscribeButton = FindViewById<Button>(Resource.Id.subscribeButton);
+
+            subscribeButton.Click += delegate 
+			{
+                FirebaseMessaging.Instance.SubscribeToTopic("news");
+                Log.Debug(TAG, "Subscribed to remote notifications");
+            };
+		}
+
+       /* This code checks the device to see if the Google Play Services APK is installed. 
+		  If it is not installed, a message is displayed in the TextBox that instructs the 
+		  user to download an APK from the Google Play Store (or to enable it in the device's system settings). */
+		
+        public bool IsPlayServicesAvailable()
+        {
+	        int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+
+	        if (resultCode != ConnectionResult.Success)
+	        {
+				if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+				{
+					msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+				}
+		        else
+		        {
+			        msgText.Text = "This device is not supported";
+			        Finish();
+		        }
+
+		        return false;
+	        }
+	        else
+	        {
+		        msgText.Text = "Google Play Services is available.";
+		        return true;
+            }
+        }
+	}
+}
+
---
+++ FCMClient/MyFirebaseIIDService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Android.App;
+using Firebase.Iid;
+using Android.Util;
+
+namespace FCMClient
+{
+  /* This service implements an OnTokenRefresh method that is invoked when the registration token is 
+	 initially created or changed. When OnTokenRefresh runs, it retrieves the latest token from the 
+	 FirebaseInstanceId.Instance.Token property (which is updated asynchronously by FCM). */
+	
+	[Service]
+	[IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+	public class MyFirebaseIIDService : FirebaseInstanceIdService
+	{
+		const string TAG = "MyFirebaseIIDService";
+
+     /* OnTokenRefresh is invoked infrequently: it is used to update the token under the following circumstances:
+        1) When the app is installed or uninstalled.
+        2) When the user deletes app data.
+        3) When the app erases the Instance ID.
+        4) When the security of the token has been compromised. */
+		
+		public override void OnTokenRefresh()
+		{
+         // In this example, the refreshed token is logged so that it can be viewed in the output window:
+
+			var refreshedToken = FirebaseInstanceId.Instance.Token;
+			Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+
+			SendRegistrationToServer(refreshedToken);
+		}
+
+     /* OnTokenRefresh also calls SendRegistrationToAppServer to associate the user's registrations token with 
+        the server-side account (if any) that is maintained by the application: */
+		
+		void SendRegistrationToServer(string token)
+		{
+			// Add custom implementation, as needed.
+		}
+
+       /* Because this implementation depends on the design of the app server, an empty method body is provided in this example. 
+          If your app server requires FCM registration information, modify SendRegistrationToAppServer to associate the user's 
+          FCM instance ID token with any server-side account maintained by your app. (Note that the token is opaque to the client app.)
+
+         When a token is sent to the app server, SendRegistrationToAppServer should maintain a boolean to indicate whether the token 
+         has been sent to the server. If this boolean is false, SendRegistrationToAppServer sends the token to the app server – otherwise, 
+         the token was already sent to the app server in a previous call. In some cases (such as this FCMClient example), 
+         the app server does not need the token; therefore, this method is not required for this example. */
+	}
+}
